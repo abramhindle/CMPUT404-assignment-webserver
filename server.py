f9bb52e34135894b97679a66ae9cadf2c6e3c8cb
Abram Hindle <abram.hindle@softwareprocess.es>
2014-01-07 22:32:23 -0700

    Update server.py
    Added Eddie in the (C)

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import SocketServer
 # coding: utf-8
 
-# Copyright 2013 Abram Hindle
+# Copyright 2013 Abram Hindle, Eddie Antonio Santos
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
