354ac49599ad3ed4b5dd12fec375167a4f36ef02
Curtis Malainey <curtis@malainey.com>
2017-01-18 20:09:15 -0700

    Fix run command

--- not-free-tests.py
+++ not-free-tests.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-# run: python freetests.py
+# run: python not-free-tests.py
 
 import urllib2
 import unittest
