5394e7cc91e649b68b68b9a2eca9ebb670f38e32
Michael Lin <michael.lin1@ibm.com>
2019-01-08 21:39:13 -0700

    refactor test suite to use python 3

--- not-free-tests.py
+++ not-free-tests.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 # Copyright 2013 Abram Hindle
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,7 +15,7 @@
 #
 # run: python not-free-tests.py
 
-import urllib2
+from urllib import request
 import unittest
 import os
 
@@ -28,26 +28,26 @@ class TestYourWebserver(unittest.TestCase):
 
     def test_get_root(self):
         url = self.baseurl + "/"
-        req = urllib2.urlopen(url, None, 3)
+        req = request.urlopen(url, None, 3)
         self.assertTrue( req.getcode()  == 200 , "200 OK Not FOUND!")
 
     def test_get_deep(self):
         url = self.baseurl + "/deep/"
-        req = urllib2.urlopen(url, None, 3)
+        req = request.urlopen(url, None, 3)
         self.assertTrue( req.getcode()  == 200 , "200 OK Not FOUND!")
 
 
     def test_get_index(self):
         url = self.baseurl + "/index.html"
-        req = urllib2.urlopen(url, None, 3)
+        req = request.urlopen(url, None, 3)
         self.assertTrue( req.getcode()  == 200 , "200 OK Not FOUND!")
 
     def test_get_404(self):
         url = self.baseurl + "/do-not-implement-this-page-it-is-not-found"
         try:
-            req = urllib2.urlopen(url, None, 3)
+            req = request.urlopen(url, None, 3)
             self.assertTrue( False, "Should have thrown an HTTP Error!")
-        except urllib2.HTTPError as e:
+        except request.HTTPError as e:
             self.assertTrue( e.getcode()  == 404 , ("404 Not FOUND! %d" % e.getcode()))
         else:
             self.assertTrue( False, "Another Error was thrown!")
@@ -56,50 +56,50 @@ class TestYourWebserver(unittest.TestCase):
         """ how secure are you? """
         url = self.baseurl + "/../../../../../../../../../../../../etc/group"
         try:
-            req = urllib2.urlopen(url, None, 3)
+            req = request.urlopen(url, None, 3)
             self.assertTrue( False, "Should have thrown an HTTP Error! [%d]" % req.getcode())
-        except urllib2.HTTPError as e:
+        except request.HTTPError as e:
             self.assertTrue( e.getcode()  == 404 , ("404 Not FOUND! %d" % e.getcode()))
         else:
             self.assertTrue( false, "Another Error was thrown!")
-        
+
     def test_css(self):
         url = self.baseurl + "/base.css"
-        req = urllib2.urlopen(url, None, 3)
+        req = request.urlopen(url, None, 3)
         self.assertTrue( req.getcode()  == 200 , "200 OK Not FOUND!")
-        self.assertTrue( req.info().gettype() == "text/css", ("Bad mimetype for css! %s" % req.info().gettype()))
+        self.assertTrue( req.info().get_content_type() == "text/css", ("Bad mimetype for css! %s" % req.info().get_content_type()))
 
     def test_html(self):
         url = self.baseurl + "/index.html"
-        req = urllib2.urlopen(url, None, 3)
+        req = request.urlopen(url, None, 3)
         self.assertTrue( req.getcode()  == 200 , "200 OK Not FOUND!")
-        self.assertTrue( req.info().gettype() == "text/html", ("Bad mimetype for html! %s" % req.info().gettype()))
+        self.assertTrue( req.info().get_content_type() == "text/html", ("Bad mimetype for html! %s" % req.info().get_content_type()))
 
     def test_hardcode(self):
         os.system("cp -r www/deep www/hardcode")
         url = self.baseurl + "/hardcode/index.html"
-        req = urllib2.urlopen(url, None, 3)
+        req = request.urlopen(url, None, 3)
         self.assertTrue( req.getcode()  == 200 , "200 OK Not FOUND! Hardcoding? /hardcode/index.html")
-        self.assertTrue( req.info().gettype() == "text/html", ("Bad mimetype for html! %s" % req.info().gettype()))
+        self.assertTrue( req.info().get_content_type() == "text/html", ("Bad mimetype for html! %s" % req.info().get_content_type()))
         url = self.baseurl + "/hardcode/"
-        req = urllib2.urlopen(url, None, 3)
+        req = request.urlopen(url, None, 3)
         self.assertTrue( req.getcode()  == 200 , "200 OK Not FOUND! Hardcoding? /hardcode/")
-        self.assertTrue( req.info().gettype() == "text/html", ("Bad mimetype for html! %s" % req.info().gettype()))
+        self.assertTrue( req.info().get_content_type() == "text/html", ("Bad mimetype for html! %s" % req.info().get_content_type()))
 
     def test_hardcode2(self):
         url = self.baseurl + "/deep.css"
         try:
-            req = urllib2.urlopen(url, None, 3)
+            req = request.urlopen(url, None, 3)
             self.assertTrue( False, "Should have thrown an HTTP Error for /deep.css!")
-        except urllib2.HTTPError as e:
+        except request.HTTPError as e:
             self.assertTrue( e.getcode()  == 404 , ("404 Not FOUND! %d" % e.getcode()))
         else:
             self.assertTrue( False, "Another Error was thrown!")
         url = self.baseurl + "/deep/deep"
         try:
-            req = urllib2.urlopen(url, None, 3)
+            req = request.urlopen(url, None, 3)
             self.assertTrue( False, "Should have thrown an HTTP Error for /deep/deep!")
-        except urllib2.HTTPError as e:
+        except request.HTTPError as e:
             self.assertTrue( e.getcode()  == 404 , ("404 Not FOUND! %d" % e.getcode()))
         else:
             self.assertTrue( False, "Another Error was thrown!")
