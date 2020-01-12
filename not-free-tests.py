73db2282da8a3544952c39c2bd37b413c9fde84d
Abram Hindle <abram.hindle@softwareprocess.es>
2020-01-11 20:37:44 -0700

    fix a change in urllib behaviour

--- not-free-tests.py
+++ not-free-tests.py
@@ -82,10 +82,14 @@ class TestYourWebserver(unittest.TestCase):
     # CMPUT404W19 did not have to pass to this
     def test_deep_no_end(self):
         url = self.baseurl + "/deep"
+        expected_url = self.baseurl + "/deep/"
         try:
             req = request.urlopen(url, None, 3)
             code = req.getcode() 
-            self.assertTrue(False, "We're not supposed to be here %s" % code)
+            if code >= 200 and code <= 299 and req.geturl() == expected_url:
+                 self.assertTrue(True, "The library has redirected for us")
+            else:
+                self.assertTrue(False, "The URL hasn't changed %s %s" % (code,req.geturl()))
         except request.HTTPError as e:
             code = e.getcode() 
             self.assertTrue( code >= 300 and code < 400, "300ish Not FOUND! %s" % code)
