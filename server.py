64fe16bd94da17bb6f263d7d3af77bffa39aa928
orackle <debangan@ualberta.ca>
2021-01-27 00:17:43 -0700

    skeleton code for handling error methods

--- server.py
+++ server.py
@@ -1,14 +1,16 @@
-#  coding: utf-8 
+#  coding: utf-8
 import socketserver
+import os.path
+from os import path
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -28,11 +30,43 @@ import socketserver
 
 
 class MyWebServer(socketserver.BaseRequestHandler):
-    
+
+    # print ("directory exists:" + str(path.exists('www')))
+
+
+
+    def handle404(self):
+        pass
+
+    def handle301(self, requestMethod):
+        print("not correct /")
+        return 1
+
     def handle(self):
         self.data = self.request.recv(1024).strip()
-        print ("Got a request of: %s\n" % self.data)
+        requestMethod = self.data.decode('utf-8')
+        check405 = requestMethod.split(" ")[0]
+
+
+        if(check405 != 'GET'):
+            response = self.handle405(requestMethod)
+        else:
+            print("go ahead")
+            check301 = requestMethod.split(" ")[2]
+            print(check301)
+            # if((check301[-1])!="/"):
+            #     self.handle301(requestMethod)
+            # else:
+            #     print("correct address" + check301)
         self.request.sendall(bytearray("OK",'utf-8'))
+
+    def handle405(self, request):
+        print("not allowed")
+        return 1
+
+
+
+
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
