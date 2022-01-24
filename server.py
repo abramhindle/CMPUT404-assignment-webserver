7de3024db7b3b46941db2ecfe8c5784ec1dfafbd
Della <humanita.1999@gmail.com>
2022-01-24 16:48:01 -0700

    attempted to send css

--- server.py
+++ server.py
@@ -32,41 +32,85 @@ from datetime import datetime
 class MyWebServer(socketserver.BaseRequestHandler):
     
     def handle(self):
+        '''
+            Main function for handling a single request
+
+        '''
+
         print("Starting server connection...")
         self.directory = 'www/'
 
-        get_res = self.do_GET()
+        # Handle get request 
+        try:
+            # Get index.html 
+            main_index = self.read_file_from_dir('index.html')
 
-        self.request.sendall(bytearray(f'{get_res}', 'utf-8'))
+            www_index = self.do_GET(200, 'text/html', main_index)
+
+            # Get base.css
+            # main_css = self.read_file_from_dir('base.css')
+            # # Get files in deep dir 
+            # deep_index = self.read_file_from_dir('deep/index.html')
+            # deep_css = self.read_file_from_dir('deep/base.css')
+
+            # # Send the responses 
+            
+            # # www_css = self.do_GET(200, 'text/css', main_css, 'Link: <www/base.css>;rel=stylesheet\r\n\r\n')
+            # # self.request.sendall(bytearray(f'{www_css}', 'utf-8'))
+
+            # www_deep_index = self.do_GET(200, 'text/html', deep_index)
+            # self.request.sendall(bytearray(f'{www_deep_index}', 'utf-8'))
+            # print('Sent deep/index.html')
+            # www_deep_css = self.do_GET(200, 'text/css', deep_css)
+            
+        except Exception as e:
+            print('[ERROR]', e)
+            self.handle_error()
+
+        finally:
+            # self.request.sendall(bytearray(f'{www_index}', 'utf-8'))
+            self.request.sendall(bytearray(f'{www_index}', 'utf-8'))
+            print('Sent index.html')
+            print("Closing server connection...")
     
     def get_date(self):
+        '''
+            Returns the current date and time
+        '''
         return datetime.now().strftime('%a, %d %b %Y %H:%M:%S')
 
-    def do_GET(self):
-        status = 'HTTP/1.1 200 OK\r\n'
-        date = 'Date:' + self.get_date() + '\r\n'
-        content_type = 'Content-Type: text/html\r\n'
-
-        body = ''
+    def read_file_from_dir(self, filename):
+        '''
+            Reads the html file 
+        '''
 
         for file in os.listdir(self.directory):
-            if file == 'index.html':
+            if file == filename:
                 path = self.directory + file
                 with open(path, 'r', encoding='utf-8') as f:
                     body = f.read()
 
-        content_len = 'Content-Length:' + str(len(body)) + '\r\n'            
-        
-        response = status + date + content_type + content_len + body
+                    return body
 
-        return response
-    
+    def do_GET(self, status_code, content_type, body, other=None):
+        status = f'HTTP/1.1 {status_code} OK\r\n'
+        date = 'Date: ' + self.get_date() + '\r\n'
+        content_type = 'Content-Type: ' + content_type + '\r\n'
+        content_len = 'Content-Length: ' + str(len(body)) + '\r\n'     
+
+        if other:
+            return status + date + content_type + content_len + other + body
+        
+        return status + date + content_type + content_len + body
+
 
     def handle_error(self):
-        self.request.sendall(bytearray("404 Not Found", 'utf-8'))
+        response = 'HTTP/1.1 404 Not Found\r\n'
+        self.request.sendall(bytearray(response, 'utf-8'))
 
 
-    
+    #TODO: close connection if you don't want to keep reading requests
+    #TODO: store a dictionary of all paths if it exists in the directory
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
