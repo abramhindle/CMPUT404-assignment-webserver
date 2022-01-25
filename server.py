3eb8424bb9b08844e6dbb0eded3d11ee13a518e6
Lidia Ataupillco <ataupill@ualberta.ca>
2022-01-24 20:44:45 -0700

    return 405 for methods that are not GET

--- server.py
+++ server.py
@@ -1,6 +1,5 @@
 #  coding: utf-8 
 import socketserver
-
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -26,13 +25,79 @@ import socketserver
 
 # try: curl -v -X GET http://127.0.0.1:8080/
 
+PROTOCOL = 'HTTP/1.1'
+OK_STATUS_CODE = '200 OK'
+N_ALLOW_METH = '405 Method Not Allowed'
 
 class MyWebServer(socketserver.BaseRequestHandler):
+    def handle_requests(self, request_info):
+        # obtain request headers
+        main = request_info[0].split()
+
+        # obtain method
+        method = main[0]
+        uri = main[1]
+        status = main[2]
+
+        parsed_info = {}
+        for val in request_info[1:]:
+            t = val.split(": ")
+            parsed_info[t[0]] = t[1]
+
+        return method, uri, parsed_info
     
+    def handle_get_res(self):
+        pass
+
     def handle(self):
+        print("address", self.client_address)
         self.data = self.request.recv(1024).strip()
+
+        request_info = self.data.decode().split("\r\n")
+
         print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+        method, uri, parsed_info = self.handle_requests(request_info)
+
+        if method == 'GET':
+            ok_request_line = f'{PROTOCOL} {OK_STATUS_CODE}\r\n'
+            # read files
+            # read html file
+            if uri.endswith('.css'):
+                self.request.sendall(bytearray(ok_request_line + f'content-Type: text/css; charset=utf-8\r\n' + f'content-Length: 48\r\n' + f'connection: close\r\n\r\n', 'utf-8'))
+                css_obj = open("www/base.css", 'rb')
+                self.request.sendall(css_obj.read() + b'\n')
+            elif uri.endswith('/'):
+                self.request.sendall(b'HTTP/1.1 200 OK\r\n' + b'content-Type: text/html; charset=utf-8\r\n' + b'content-Length: 471\r\n' + b'connection: close\r\n\r\n')
+                file_obj = open("www/index.html", 'rb')
+                self.request.sendall(file_obj.read() + b'\n')
+        else:
+            # method not handled
+            nallow_request_line = f'{PROTOCOL} {N_ALLOW_METH}\r\n'
+            self.request.sendall(bytearray(nallow_request_line, 'utf-8'))
+            header_nallow_meth = 'Content-Type:text/plain\r\nContent-Length: 0\r\nConnection: close\r\n\r\n'
+            self.request.sendall(bytearray(header_nallow_meth, 'utf-8'))
+
+        # CSS
+        # self.request.sendall(b'HTTP/1.1 200 OK\r\n' + b'content-Type: text/css\r\n' + b'content-Length: 48\r\n' + b'Connection: keep-alive\r\n\r\n')
+        # css_obj = open("www/base.css", 'rb')
+        # self.request.sendall(css_obj.read())
+
+        # self.request.sendall(bytearray(css_obj.read() + '\n','utf-8'))
+
+
+        # root_html = bytearray(file_obj.read() + '\n','utf-8')
+
+        ##### USE SENDFILE
+
+        # send response
+        self.request.sendall(b'HTTP/1.1 200 OK\r\n' + b'content-Type: text/html; charset=utf-8\r\n' + b'content-Length: 471\r\n\r\n')
+        # self.request.sendall(b'\r\n\r\n')
+
+        self.finish()
+    
+    def decode(self, data):
+        return data.decode()
+
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
