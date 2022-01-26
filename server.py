6fd4d73d66e225106cab84a94fef8db2967675cd
Lidia Ataupillco <ataupill@ualberta.ca>
2022-01-25 22:24:16 -0700

    server is working

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 #  coding: utf-8 
 import socketserver
+import re
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -28,6 +29,17 @@ import socketserver
 PROTOCOL = 'HTTP/1.1'
 OK_STATUS_CODE = '200 OK'
 N_ALLOW_METH = '405 Method Not Allowed'
+HTML_FILE_NAME = '/index.html'
+REDIR_STATUS_CODE = '301 Moved Permanently'
+NFOUND_STATUS_CODE = '404 Not Found'
+
+def construct_header_str(headers):
+    header_str = ''
+    for header in headers:
+        header_str += f'{header}:{headers[header]}\r\n'
+    
+    header_str += '\r\n'
+    return header_str.encode('utf-8')
 
 class MyWebServer(socketserver.BaseRequestHandler):
     def handle_requests(self, request_info):
@@ -46,11 +58,19 @@ class MyWebServer(socketserver.BaseRequestHandler):
 
         return method, uri, parsed_info
     
-    def handle_get_res(self):
-        pass
+    def send_response(self, request, bbody, status, headers):
+        # send request line
+        request_line = f'{PROTOCOL} {status}\r\n'.encode('utf-8')
+        request.sendall(request_line)
+        
+        # send headers
+        header_str = construct_header_str(headers)
+        request.sendall(header_str)
+
+        # send body 
+        request.sendall(bbody + b'\n')
 
     def handle(self):
-        print("address", self.client_address)
         self.data = self.request.recv(1024).strip()
 
         request_info = self.data.decode().split("\r\n")
@@ -59,39 +79,104 @@ class MyWebServer(socketserver.BaseRequestHandler):
         method, uri, parsed_info = self.handle_requests(request_info)
 
         if method == 'GET':
-            ok_request_line = f'{PROTOCOL} {OK_STATUS_CODE}\r\n'
-            # read files
-            # read html file
-            if uri.endswith('.css'):
-                self.request.sendall(bytearray(ok_request_line + f'content-Type: text/css; charset=utf-8\r\n' + f'content-Length: 48\r\n' + f'connection: close\r\n\r\n', 'utf-8'))
-                css_obj = open("www/base.css", 'rb')
-                self.request.sendall(css_obj.read() + b'\n')
-            elif uri.endswith('/'):
-                self.request.sendall(b'HTTP/1.1 200 OK\r\n' + b'content-Type: text/html; charset=utf-8\r\n' + b'content-Length: 471\r\n' + b'connection: close\r\n\r\n')
-                file_obj = open("www/index.html", 'rb')
-                self.request.sendall(file_obj.read() + b'\n')
+            if uri == '/base.css':
+                # read css file
+                css_obj = open(f"www{uri}", 'r')
+                bbody = css_obj.read().encode('utf-8')
+
+                # generate headers
+                headers = {
+                    'Content-Type': 'text/css; charset=utf-8',
+                    'Content-Length': f'{len(bbody)}',
+                    'connection': 'close'
+                }
+                self.send_response(self.request, bbody, OK_STATUS_CODE, headers)
+            # check if we need to consider /index.html/
+            elif uri == '/' or uri == '/index.html':
+                # read html file
+                file_obj = open(f"www{HTML_FILE_NAME}", 'r')
+                bbody = file_obj.read().encode('utf-8')
+
+                # generate headers
+                headers = {
+                    'Content-Type': 'text/html; charset=utf-8',
+                    'Content-Length': f'{len(bbody)}',
+                    'connection': 'close'
+                }
+
+                self.send_response(self.request, bbody, OK_STATUS_CODE, headers)                
+            elif uri == '/deep/deep.css':
+                # read css file
+                css_obj = open(f"www{uri}", 'r')
+                bbody = css_obj.read().encode('utf-8')
+
+                # generate headers
+                headers = {
+                    'Content-Type': 'text/css; charset=utf-8',
+                    'Content-Length': f'{len(bbody)}',
+                    'connection': 'close'
+                }
+
+                # send message
+                self.send_response(self.request, bbody, OK_STATUS_CODE, headers)                
+            elif re.match(r"^/deep/?$", uri) is not None or re.match(r'^/deep/index.html/?$', uri) is not None:
+                if uri.endswith('/'):
+                    # read html file
+                    file_obj = open(f"www/deep/{HTML_FILE_NAME}", 'r')
+                    bbody = file_obj.read().encode('utf-8')
+
+                    headers = {
+                        'Content-Type': 'text/html; charset=utf-8',
+                        'Content-Length': f'{len(bbody)}',
+                        'Connection': 'close'
+                    }
+                    
+                    self.send_response(self.request, bbody, OK_STATUS_CODE, headers)
+                else:
+                    # redirect
+                    new_address = 'http://127.0.0.1:8080/deep/'
+
+                    bbody = f'File moved'.encode('utf-8')
+
+                    headers = {
+                        'Content-Type': 'text/plain; charset=utf-8',
+                        'Content-Length': f'{len(bbody)}',
+                        'Connection': 'keep-alive',
+                        'Location': f'{new_address}'
+                    }
+
+                    # send message
+                    self.send_response(self.request, bbody, REDIR_STATUS_CODE, headers)
+            else:
+                nfound_message = f'{uri.capitalize()} path not found.'.encode('utf-8')
+                
+                # generate headers
+                headers = {
+                    'Content-Type': 'text/plain; charset=utf-8',
+                    'Content-Length': f'{len(nfound_message)}',
+                    'connection': 'keep-alive'
+                }
+
+                # send message
+                self.send_response(self.request, nfound_message, NFOUND_STATUS_CODE, headers)
         else:
-            # method not handled
-            nallow_request_line = f'{PROTOCOL} {N_ALLOW_METH}\r\n'
-            self.request.sendall(bytearray(nallow_request_line, 'utf-8'))
-            header_nallow_meth = 'Content-Type:text/plain\r\nContent-Length: 0\r\nConnection: close\r\n\r\n'
-            self.request.sendall(bytearray(header_nallow_meth, 'utf-8'))
+            # Response for method not handled
+            # create message
+            nallow_message = f'Method {method} not allowed. You can only make GET requests.\n'.encode('utf-8')
+            
+            # generate headers
+            headers = {
+                'Content-Type': 'text/html; charset=utf-8',
+                'Content-Length': f'{len(nallow_message)}',
+                'connection': 'close'
+            }
 
-        # CSS
-        # self.request.sendall(b'HTTP/1.1 200 OK\r\n' + b'content-Type: text/css\r\n' + b'content-Length: 48\r\n' + b'Connection: keep-alive\r\n\r\n')
-        # css_obj = open("www/base.css", 'rb')
-        # self.request.sendall(css_obj.read())
+            # send message
+            self.send_response(self.request, nallow_message, N_ALLOW_METH, headers)                
 
-        # self.request.sendall(bytearray(css_obj.read() + '\n','utf-8'))
-
-
-        # root_html = bytearray(file_obj.read() + '\n','utf-8')
+            
 
         ##### USE SENDFILE
-
-        # send response
-        self.request.sendall(b'HTTP/1.1 200 OK\r\n' + b'content-Type: text/html; charset=utf-8\r\n' + b'content-Length: 471\r\n\r\n')
-        # self.request.sendall(b'\r\n\r\n')
 
         self.finish()
     
