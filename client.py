7cc09e8541f0c493e04648a839f3120d9ac523ed
Kezziah Ayuno <kezziahayuno@Kezziahs-MacBook-Air.local>
2023-01-10 15:58:50 -0700

    client.py try added

--- client.py
+++ client.py
@@ -4,10 +4,13 @@ HOST, PORT = "localhost", 8080
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-s.connect((HOST, PORT))
+try:
+    s.connect((HOST, PORT))
 
-s.sendall('Are you there?'.encode('UTF-8'))
-received = str(s.recv(1024).decode())
+    s.sendall('Are you there?'.encode('UTF-8'))
+    received = str(s.recv(1024).decode())
 
-print("Sent:     {}".format('Are you there?'))
-print("Received: {}".format(received))
+    print("Sent:     {}".format('Are you there?'))
+    print("Received: {}".format(received))
+except: 
+    print("Connection failed.")
