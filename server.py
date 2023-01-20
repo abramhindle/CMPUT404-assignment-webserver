722f6d8b2818425f655559a875e47802caf709d7
Kezziah Ayuno <kezziahayuno@Kezziahs-MacBook-Air.local>
2023-01-19 21:58:22 -0700

    added my name to license

--- server.py
+++ server.py
@@ -2,7 +2,7 @@
 import socketserver
 import os
 
-# Copyright 2013 Abram Hindle, Eddie Antonio Santos
+# Copyright 2013 Abram Hindle, Eddie Antonio Santos, Kezziah Camille Ayuno
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
