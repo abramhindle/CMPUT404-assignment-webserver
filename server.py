9b8180005bd8658ee59e1a05bd87dec99f564b3c
kyle <kyle@Ubuntu-22.04.myguest.virtualbox.org>
2023-01-21 01:31:20 -0700

    added basic abilities to serve html and css files to firefox etc

--- server.py
+++ server.py
@@ -32,7 +32,45 @@ class MyWebServer(socketserver.BaseRequestHandler):
     def handle(self):
         self.data = self.request.recv(1024).strip()
         print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+
+        # https://emalsha.wordpress.com/2016/11/24/how-create-http-server-using-python-socket-part-ii/
+
+        # parse the arguments
+        args = self.data.decode('utf-8').split(' ') # split the recieved data
+        method = args[0] # this is like GET or POST
+        requested = args[1] # get the requested file/folder
+
+        filepath = requested.lstrip('/')
+        if filepath == '': # no filepath specified, go to default index.html
+            filepath = 'www/index.html'
+        else: # add www/ to filepath as is never specified when calling the URL
+            filepath = 'www/' + filepath
+
+        # This will try to find, read and send the desired file back to the connected client. If no such file is found then we return 404 Not Found
+        try:
+            if (filepath.endswith('.html')):
+                mimetype = 'text/html'
+            elif (filepath.endswith('.css')):
+                mimetype = 'text/css'
+            else: # html not specified in file path but could still be desired
+                mimetype = 'text/html'
+                filepath += 'index.html'
+            
+            # will return an error if file does not exist -> will direct user to 404 Not Found
+            file = open(filepath, 'rb')
+            response = file.read()
+            file.close()
+
+            header = 'HTTP/1.1 200 OK\nContent-Type: ' + mimetype + '\n\n'
+
+        except Exception as e:
+            header = 'HTTP/1.1 404 Not Found\n\n'
+            response = '<html><body><h3>Error 404: File not found</h3></body></html>'.encode('utf-8')
+
+        # Package up our final response to send to the client
+        final_response = header.encode('utf-8')
+        final_response += response
+        self.request.sendall(final_response)
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
