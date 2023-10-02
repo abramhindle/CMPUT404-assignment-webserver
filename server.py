ca5c5c9fdf8eb331ef1f12cb6cf10fd3cb3a7dd8
ShalomiH <com.phyllo@gmail.com>
2023-10-02 11:24:58 -0600

    server complete

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 #  coding: utf-8 
-import socketserver
+import socketserver, os
 
+# Copyright 2023 Shalomi Hron
+#
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -32,7 +34,83 @@ class MyWebServer(socketserver.BaseRequestHandler):
     def handle(self):
         self.data = self.request.recv(1024).strip()
         print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+
+        #self.data = None # TODO: test unreadable request
+        if self.data == None:
+            # Badly formatted request
+            self.request.sendall(bytearray("HTTP/1.1 400 Bad Request\r\nContent-Type: text/html\r\n\r\n"+"<body><h1>400: Bad Request</h1></body>",'utf-8'))
+
+        self.plainData = self.data.decode().split()
+        method, path, protocol = self.plainData[0:3]
+
+        # Confirm method can be handled
+        if self.plainData[0] == "GET":
+            self.handleGet(path)
+        else:
+            # Return 405 error for any method that you cannot handle
+            self.request.sendall(bytearray("HTTP/1.1 405 Method Not Allowed\r\nContent-Type: text/html\r\n\r\n"+"<body><h1>405: Method Not Allowed</h1></body>",'utf-8'))
+        
+        #self.request.sendall(bytearray("OK",'utf-8'))
+
+
+    def handleGet(self, path):
+        # TODO: remove
+        # Get pairs of header names and what they are set to
+        # headersList = list(self.plainData[3::])
+        # headers = dict(zip(headersList[::2], headersList[1::2]))
+        # print(headers)
+
+        pathEnd = path.split("/")[-1] # Is the deepest path of the path a file?
+        
+        # Check if we need a 301 code to correct the path ending where no file is requested.
+        if (path[-1] != "/") and (not "." in pathEnd):
+            path += "/"
+            self.request.sendall(bytearray("HTTP/1.1 301 Moved Permanently\r\nLocation: "+path+"\r\n",'utf-8'))
+        else:
+            # For correct path endings, confirm the rest of the path is correct.
+            path = "/www" + path # the webserver serves files from ./www
+            refDirectory = os.path.dirname(os.path.realpath(__file__))
+            fullLocation = refDirectory + path
+
+            # If the path is correct, then serve the specified file, or the index.html file if none is specified
+            if os.path.exists(fullLocation):
+                self.serveFiles(path, fullLocation)
+            else:
+                # The directory itself does not exist
+                self.request.sendall(bytearray("HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\n\r\n"+"<body><h1>404: Not Found</h1></body>",'utf-8'))
+
+
+    def serveFiles(self, path, fullLocation):
+        if "." in path:
+            # Try serving the file
+            try:
+                with open(fullLocation, 'r') as file:
+                    file_content = file.read() 
+
+                # Send the proper file mime-types
+                fileType = path.split(".")
+                self.request.sendall(bytearray("HTTP/1.1 200 OK\r\nContent-Type: text/" + fileType[1]+"\r\n\r\n"+file_content,'utf-8'))
+            except FileNotFoundError:
+                print("Error: the path is invalid / the file does not exist.")
+                self.request.sendall(bytearray("HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\n\r\n"+"<body><h1>404: Not Found</h1></body>",'utf-8'))
+            except:
+                print("Error: Unable to load the file.")
+                # 500 Internal Server Error
+                self.request.sendall(bytearray("HTTP/1.1 500 Internal Server Error\r\n",'utf-8'))
+        else:
+            # If no file was specified, try serving index.html
+            try:
+                with open(fullLocation + "index.html", 'r') as file:
+                    file_content = file.read() 
+                self.request.sendall(bytearray("HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n"+file_content,'utf-8'))
+            except FileNotFoundError:
+                print("Error: the path is invalid / the file does not exist.")
+                self.request.sendall(bytearray("HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\n\r\n"+"<body><h1>404: Not Found</h1></body>",'utf-8'))
+            except:
+                print("Error: Unable to load the file.")
+                # 500 Internal Server Error
+                self.request.sendall(bytearray("HTTP/1.1 500 Internal Server Error\r\n",'utf-8'))
+
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
@@ -44,3 +122,4 @@ if __name__ == "__main__":
     # Activate the server; this will keep running until you
     # interrupt the program with Ctrl-C
     server.serve_forever()
+
